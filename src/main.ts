ddfac2d602130b24a922b22707cf606482e6220d
injashkin <injashkin@gmail.com>
2023-03-16 01:34:14 +0300

    Output all data

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ const options = {
   per_page: 10,
 };
 
+type Repo = {
+  name: string;
+  html_url: string;
+  description: string;
+  updated_at: string;
+  language: string;
+};
+
 type JSONResponse = {
   total_count: number;
+  items: Repo[];
 };
 
 document.addEventListener("click", handleClick);
@@ -20,7 +29,6 @@ function handleClick(e: Event) {
     const queryString = inputQuery.value;
     const url = getUrl(queryString, options);
     getAndShow(url);
-    
   }
 }
 
@@ -34,11 +42,38 @@ async function fetchData<T>(url: string): Promise<T> {
   return await response.json();
 }
 
+function formatDate(date: string) {
+  return date.slice(0, 10).split("-").reverse().join("-");
+}
+
 function outputList(data: JSONResponse) {
-  const { total_count } = data;
+  const { total_count, items } = data;
 
   const output = <HTMLElement>document.querySelector(".search__output");
-  output.innerHTML = `${total_count}`;
+
+  output.innerHTML = `
+  <div><span>Найдено ${total_count} репозиториев</span></div>
+  <ol>
+  `;
+
+  for (let item of items) {
+    const { name, html_url, description, updated_at, language } = item;
+
+    const updated = formatDate(updated_at);
+
+    output.innerHTML += `
+    <li>
+      <a title="Имя репозитория" href="${html_url}">${name}</a>
+      <span title="Язык программирования" class="search__lang">${language}</span>
+      <span title="Дата последнего обновления" class="search__date">${updated}</span>
+      <span title="Описания репозитория">${description}</span>
+    </li>
+    `;
+  }
+
+  output.innerHTML += `
+  </ol>
+  `;
 }
 
 type optionsProp = {
@@ -48,7 +83,7 @@ type optionsProp = {
 function getUrl(str: string, options: optionsProp) {
   const { per_page } = validate(options);
 
-  let url = `https://api.github.com/search/repositories?q=${str}&per_page=${per_page}`;
+  let url = `https://api.github.com/search/repositories?q=${str}&per_page=10`;
 
   return url;
 }
