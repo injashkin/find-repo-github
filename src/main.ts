cc8cddbfb7acbbf9be8d673afffcfab6ca2dd137
injashkin <injashkin@gmail.com>
2023-03-15 22:46:37 +0300

    Show the number of repositories found

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import "./style.css";
 
+const options = {
+  per_page: 10,
+};
+
+type JSONResponse = {
+  total_count: number;
+};
+
 document.addEventListener("click", handleClick);
 
 // ====================== Functions ==========================
@@ -9,19 +17,55 @@ function handleClick(e: Event) {
 
   if (target.closest(".search__submit")) {
     const inputQuery = <HTMLFormElement>target.previousElementSibling;
-
     const queryString = inputQuery.value;
-
-    console.log(queryString);
-    fetchData();
+    const url = getUrl(queryString, options);
+    getAndShow(url);
+    
   }
 }
 
-async function fetchData() {
-  let url = `https://api.github.com/search/repositories?q=toxin`;
+async function getAndShow(url: string) {
+  const fetchedData = await fetchData<JSONResponse>(url);
+  outputList(fetchedData);
+}
 
+async function fetchData<T>(url: string): Promise<T> {
   let response = await fetch(url);
-  
-  let fetchedData = await response.json();
-  console.log(fetchedData);
+  return await response.json();
+}
+
+function outputList(data: JSONResponse) {
+  const { total_count } = data;
+
+  const output = <HTMLElement>document.querySelector(".search__output");
+  output.innerHTML = `${total_count}`;
+}
+
+type optionsProp = {
+  per_page: number;
+};
+
+function getUrl(str: string, options: optionsProp) {
+  const { per_page } = validate(options);
+
+  let url = `https://api.github.com/search/repositories?q=${str}&per_page=${per_page}`;
+
+  return url;
+}
+
+function validate(data: optionsProp) {
+  const { per_page } = data;
+
+  const result = {
+    per_page: 30,
+    message: { per_page: "" },
+  };
+
+  if (per_page > 100) {
+    result.per_page = 100;
+    result.message.per_page =
+      "Количество результатов на страницу не может превышать 100";
+  }
+
+  return result;
 }
